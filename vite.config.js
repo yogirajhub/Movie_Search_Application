66cc27d2ba85c11aa81fd638143b5c5fe00e3641
Yogiraj Gautam <yogirajgautam18@gmail.com>
2025-07-15 11:08:06 +0530

    add data in vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Movie_Search_Application/', // 👈 Must match your GitHub repo name
   plugins: [react()],
 })
